5c53a2f612af9fbd7b77bbbb98b7ebb385679943
MoonWX <wangxiao3027@vip.qq.com>
2023-08-17 13:52:01 +0800

    delete all

--- 1.js
+++ 1.js
@@ -1,13 +1,1 @@
-//同步执行
-console.log(111);
-function fn(cb) {
-    setTimeout(() => {
-        console.log(333);
-        cb();
-    });
-}
-console.log(222)
-//异步执行
-fn(function(){
-    console.log("异步之后执行的代码");
-})
+//delete all
