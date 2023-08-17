551c462eb5a2728cf90a1ad755b80038f008c847
MoonWX <wangxiao3027@vip.qq.com>
2023-08-17 13:55:32 +0800

    Revert "delete all"
    This reverts commit 5c53a2f612af9fbd7b77bbbb98b7ebb385679943.

--- 1.js
+++ 1.js
@@ -1,1 +1,13 @@
-//delete all
+//同步执行
+console.log(111);
+function fn(cb) {
+    setTimeout(() => {
+        console.log(333);
+        cb();
+    });
+}
+console.log(222)
+//异步执行
+fn(function(){
+    console.log("异步之后执行的代码");
+})
